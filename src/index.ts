513865d9bb7fe41b90628359744a4b835c3817e7
Robin Tail <robin_tail@me.com>
2024-08-06 23:50:41 +0200

    REF: types from json schema.

--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,20 @@ import {
   type TSESLint,
   type JSONSchema,
 } from "@typescript-eslint/utils";
-import { path, flip, partition, reject, startsWith } from "ramda";
-import type { PackageJson } from "type-fest";
+import {
+  path,
+  flip,
+  partition,
+  reject,
+  startsWith,
+  xprod,
+  fromPairs,
+} from "ramda";
+import type { FromSchema } from "json-schema-to-ts";
 
 const messages = {
   prohibited: "Importing {{name}} is not allowed.",
   typeOnly: "Only 'import type' syntax is allowed for {{name}}.",
-};
-
-type Variant = boolean | "typeOnly";
-type Items = Record<"production" | "requiredPeers" | "optionalPeers", Variant>;
-
-type Options = Partial<Items> & {
-  manifest: PackageJson.PackageJsonStandard;
-  typeOnly?: string[];
-};
-
-const defaults: Options = {
-  manifest: {},
-  production: true,
-  requiredPeers: true,
-  optionalPeers: "typeOnly",
 };
 
 const excludeTypes = reject(startsWith("@types/"));
@@ -33,27 +26,52 @@ const getPackageName = (subject: string) =>
     .slice(0, subject.startsWith("@") ? 2 : 1)
     .join("/");
 
-const itemSchema: JSONSchema.JSONSchema4 = {
+const valueSchema = {
   oneOf: [{ type: "boolean" }, { type: "string", enum: ["typeOnly"] }],
+} as const satisfies JSONSchema.JSONSchema4;
+
+const itemsSchema = {
+  type: "object",
+  properties: fromPairs(
+    xprod(["production", "optionalPeers", "requiredPeers"] as const, [
+      valueSchema,
+    ]),
+  ),
+} as const satisfies JSONSchema.JSONSchema4;
+
+const manifestSchema = {
+  type: "object",
+  properties: fromPairs(
+    xprod(
+      ["dependencies", "peerDependencies", "peerDependenciesMeta"] as const,
+      [{ type: "object" } as const],
+    ),
+  ),
+} as const satisfies JSONSchema.JSONSchema4;
+
+const schema = {
+  type: "object",
+  properties: {
+    manifest: manifestSchema,
+    typeOnly: { type: "array", items: { type: "string" } },
+    ...itemsSchema.properties,
+  },
+  additionalProperties: false,
+  required: ["manifest"],
+} as const satisfies JSONSchema.JSONSchema4;
+
+const defaults: FromSchema<typeof schema> = {
+  manifest: {},
+  production: true,
+  requiredPeers: true,
+  optionalPeers: "typeOnly",
 };
 
 const theRule = ESLintUtils.RuleCreator.withoutDocs({
   meta: {
     messages,
     type: "problem",
-    schema: [
-      {
-        type: "object",
-        properties: {
-          manifest: { type: "object" },
-          typeOnly: { type: "array", items: { type: "string" } },
-          production: itemSchema,
-          requiredPeers: itemSchema,
-          optionalPeers: itemSchema,
-        },
-        required: ["manifest"],
-      },
-    ],
+    schema: [schema],
   },
   defaultOptions: [defaults],
   create: (
