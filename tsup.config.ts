0d7b6cc19e415b181ea34e415d4a1dd72a1072fd
Anna Bocharova <anna.bocharova@deliveryhero.com>
2024-08-07 11:52:33 +0200

    Min node 18.18.0.

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,8 @@
+import semver from "semver";
 import { defineConfig } from "tsup";
+import { engines } from "./package.json";
+
+const minNode = semver.minVersion(engines.node)!;
 
 export default defineConfig({
   format: ["cjs", "esm"],
@@ -8,7 +12,7 @@ export default defineConfig({
   clean: true,
   dts: false,
   minify: true,
-  target: "node18.18.0",
+  target: `node${minNode.major}.${minNode.minor}.${minNode.patch}`,
   cjsInterop: true,
   skipNodeModulesBundle: true,
 });
