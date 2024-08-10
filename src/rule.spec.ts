70d318c68ced739250d831925bf7f9b24af8a8a3
Anna Bocharova <anna.bocharova@deliveryhero.com>
2024-08-10 23:41:51 +0200

    Also makeSetup().

--- src/rule.spec.ts
+++ src/rule.spec.ts
@@ -7,103 +7,100 @@ mock.module("node:fs", () => ({
   readFileSync: readerMock,
 }));
 
-const mockEnv = (env: object) =>
+const makeSetup = (env: object) => () =>
   readerMock.mockReturnValueOnce(JSON.stringify(env));
 
 new Runner("dependencies", rule, {
   // Valid
   "regular import of prod dependency": {
     code: `import {} from "eslint"`,
-    setup: () => mockEnv({ dependencies: { eslint: "" } }),
+    setup: makeSetup({ dependencies: { eslint: "" } }),
   },
   "regular import of explicitly enabled prod dependencies": {
     code: `import {} from "eslint"`,
     options: [{ production: true }],
-    setup: () => mockEnv({ dependencies: { eslint: "" } }),
+    setup: makeSetup({ dependencies: { eslint: "" } }),
   },
   "regular import of required peer dependency": {
     code: `import {} from "eslint"`,
-    setup: () => mockEnv({ peerDependencies: { eslint: "" } }),
+    setup: makeSetup({ peerDependencies: { eslint: "" } }),
   },
   "type import of optional peer dependency": {
     code: `import type {} from "eslint"`,
-    setup: () =>
-      mockEnv({
-        peerDependencies: { eslint: "" },
-        peerDependenciesMeta: { eslint: { optional: true } },
-      }),
+    setup: makeSetup({
+      peerDependencies: { eslint: "" },
+      peerDependenciesMeta: { eslint: { optional: true } },
+    }),
   },
   "type import of unlisted type only dependency": {
     code: `import type {} from "eslint"`,
     options: [{ typeOnly: ["eslint"] }],
-    setup: () => mockEnv({ dependencies: {} }),
+    setup: makeSetup({ dependencies: {} }),
   },
   "type import of prod dependency": {
     code: `import type {} from "eslint"`,
-    setup: () => mockEnv({ dependencies: { eslint: "" } }),
+    setup: makeSetup({ dependencies: { eslint: "" } }),
   },
   "import a path of prod dependency": {
     code: `import {} from "eslint/something/useful.js"`,
-    setup: () => mockEnv({ dependencies: { eslint: "" } }),
+    setup: makeSetup({ dependencies: { eslint: "" } }),
   },
   "import of scoped required peer dependency": {
     code: `import {} from "@eslint/js/something"`,
-    setup: () => mockEnv({ peerDependencies: { "@eslint/js": "" } }),
+    setup: makeSetup({ peerDependencies: { "@eslint/js": "" } }),
   },
   "import of local path": {
     code: `import {} from "./some/path.js"`,
-    setup: () => mockEnv({}),
+    setup: makeSetup({}),
   },
   "import of built-in module": {
     code: `import { readFileSync } from "node:fs"`,
-    setup: () => mockEnv({}),
+    setup: makeSetup({}),
   },
   "import of something explicitly ignored by regex": {
     code: `import {} from "fancy-module"`,
     options: [{ ignore: ["^fancy-\\w+$"] }],
-    setup: () => mockEnv({}),
+    setup: makeSetup({}),
   },
   // Invalid
   "regular import of unlisted dependency": {
     code: `import {} from "eslint"`,
-    setup: () => mockEnv({ dependencies: {} }),
+    setup: makeSetup({ dependencies: {} }),
     errors: [{ messageId: "prohibited" }],
   },
   "regular import of explicitly disabled prod dependency": {
     code: `import {} from "eslint"`,
     options: [{ production: false }],
-    setup: () => mockEnv({ dependencies: { eslint: "" } }),
+    setup: makeSetup({ dependencies: { eslint: "" } }),
     errors: [{ messageId: "prohibited" }],
   },
   "regular import of optional peer dependency": {
     code: `import {} from "eslint"`,
-    setup: () =>
-      mockEnv({
-        peerDependencies: { eslint: "" },
-        peerDependenciesMeta: { eslint: { optional: true } },
-      }),
+    setup: makeSetup({
+      peerDependencies: { eslint: "" },
+      peerDependenciesMeta: { eslint: { optional: true } },
+    }),
     errors: [{ messageId: "typeOnly" }],
   },
   "regular import of unlisted type only dependency": {
     code: `import {} from "eslint"`,
     options: [{ typeOnly: ["eslint"] }],
-    setup: () => mockEnv({}),
+    setup: makeSetup({}),
     errors: [{ messageId: "typeOnly" }],
   },
   "import of built-in module when missing in ignore list": {
     code: `import {} from "node:fs"`,
     options: [{ ignore: ["^fancy-\\w+$"] }],
-    setup: () => mockEnv({}),
+    setup: makeSetup({}),
     errors: [{ messageId: "prohibited" }],
   },
   demo: {
     code: `import {factory} from "typescript"; import {format} from "prettier";`,
-    setup: () =>
-      mockEnv({
-        devDependencies: { typescript: "^5" },
-        peerDependencies: { prettier: "^3" },
-        peerDependenciesMeta: { prettier: { optional: true } },
-      }),
+    setup: makeSetup({
+      devDependencies: { typescript: "^5" },
+      peerDependencies: { prettier: "^3" },
+      peerDependenciesMeta: { prettier: { optional: true } },
+    }),
     errors: [{ messageId: "prohibited" }, { messageId: "typeOnly" }],
   },
 }).run();
