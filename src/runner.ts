ff0c5838c266f4abaaaa2bf7e12876bba290aff6
Anna Bocharova <anna.bocharova@deliveryhero.com>
2024-08-10 10:44:58 +0200

    Ref: extracting for neatness.

--- src/runner.ts
+++ src/runner.ts
@@ -6,6 +6,33 @@ import {
   type ValidTestCase,
 } from "@typescript-eslint/rule-tester";
 import type { RuleModule } from "@typescript-eslint/utils/ts-eslint";
+import { has, mapObjIndexed, partition, values } from "ramda";
+
+type SomeCase<Options extends readonly unknown[], MessageId extends string> =
+  | ValidTestCase<Options>
+  | InvalidTestCase<MessageId, Options>;
+
+type Scenario<
+  Options extends readonly unknown[],
+  MessageId extends string,
+> = SomeCase<Options, MessageId> & {
+  before?: () => void;
+};
+
+const toCase = <Options extends readonly unknown[], MessageId extends string>(
+  { before, ...v }: Scenario<Options, MessageId>,
+  name: string,
+): SomeCase<Options, MessageId> => ({
+  name,
+  ...v,
+});
+
+const isInvalid = <
+  Options extends readonly unknown[],
+  MessageId extends string,
+>(
+  entry: SomeCase<Options, MessageId>,
+): entry is InvalidTestCase<MessageId, Options> => has("errors", entry);
 
 export class Runner<
   Options extends readonly unknown[],
@@ -14,12 +41,7 @@ export class Runner<
   constructor(
     protected title: string,
     protected ruleModule: RuleModule<MessageId, Options>,
-    protected scenarios: Record<
-      string,
-      (ValidTestCase<Options> | InvalidTestCase<MessageId, Options>) & {
-        before?: () => void;
-      }
-    >,
+    protected scenarios: Record<string, Scenario<Options, MessageId>>,
   ) {
     RuleTester.afterAll = afterAll;
     RuleTester.describe = describe;
@@ -34,19 +56,10 @@ export class Runner<
     const tester = new RuleTester({
       languageOptions: { parser },
     });
-    const [valid, invalid] = Object.keys(this.scenarios).reduce<
-      [ValidTestCase<Options>[], InvalidTestCase<MessageId, Options>[]]
-    >(
-      ([withoutErrors, withErrors], name) => {
-        const { before, ...scenario } = this.scenarios[name];
-        ("errors" in scenario ? withErrors : withoutErrors).push({
-          name,
-          ...scenario,
-        });
-        return [withoutErrors, withErrors];
-      },
-      [[], []],
-    );
+
+    const cases = values(mapObjIndexed(toCase, this.scenarios));
+    const [invalid, valid] = partition(isInvalid, cases);
+
     tester.run(this.title, this.ruleModule, { valid, invalid });
   }
 }
