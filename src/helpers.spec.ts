8ac848e31c166a98d343989d079efb1c04cd8c94
Robin Tail <robin_tail@me.com>
2024-08-13 20:17:42 +0200

    Test for getManifest().

--- src/helpers.spec.ts
+++ src/helpers.spec.ts
@@ -1,5 +1,5 @@
-import { describe, expect, it } from "bun:test";
-import { getName } from "./helpers.ts";
+import { describe, expect, it, mock } from "bun:test";
+import { getManifest, getName } from "./helpers.ts";
 
 describe("Helpers", () => {
   describe("getName()", () => {
@@ -10,6 +10,22 @@ describe("Helpers", () => {
       ["@eslint/js/some/path", "@eslint/js"],
     ])("returns the package name for %s", (subj, exp) => {
       expect(getName(subj)).toBe(exp);
+    });
+  });
+
+  describe("getManifest()", async () => {
+    const readerMock = mock(() => `{ "devDependencies": { "eslint": "^9" } }`);
+    const actualFs = await import("node:fs");
+    mock.module("node:fs", () => ({
+      ...actualFs,
+      readFileSync: readerMock,
+    }));
+
+    it("should read the package.json file from the specified path", () => {
+      expect(getManifest("./some/path")).toEqual({
+        devDependencies: { eslint: "^9" },
+      });
+      expect(readerMock).toHaveBeenCalledWith("some/path/package.json", "utf8");
     });
   });
 });
