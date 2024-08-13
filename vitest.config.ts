34fcbf670647c85735a4b7a3d88c78583cfb6752
Anna Bocharova <anna.bocharova@deliveryhero.com>
2024-08-14 00:22:58 +0200

    Enabling coverage.

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    pool: "threads",
+    testTimeout: 10000,
+    reporters: "basic",
+    coverage: {
+      provider: "istanbul",
+      reporter: [["text", { maxCols: 120 }], "json-summary", "html", "lcov"],
+      include: ["src/**"],
+    },
+  },
+});
