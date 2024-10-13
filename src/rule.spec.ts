6ee7f77e1f093080569cb86d423aec0fb33f1212
Robin Tail <robin_tail@me.com>
2024-10-13 23:51:47 +0200

    Minor: naming.

--- src/rule.spec.ts
+++ src/rule.spec.ts
@@ -4,7 +4,7 @@ import { RuleTester } from "@typescript-eslint/rule-tester";
 import { readerMock } from "../mocks/fs.ts";
 import { rule } from "./rule";
 
-const makeSetup = (env: object) => () =>
+const makeBefore = (env: object) => () =>
   readerMock.mockReturnValueOnce(JSON.stringify(env));
 
 RuleTester.afterAll = afterAll;
@@ -18,23 +18,23 @@ tester.run("dependencies", rule, {
     {
       name: "regular import of prod dependency",
       code: `import {} from "eslint"`,
-      before: makeSetup({ dependencies: { eslint: "" } }),
+      before: makeBefore({ dependencies: { eslint: "" } }),
     },
     {
       name: "regular import of explicitly enabled prod dependencies",
       code: `import {} from "eslint"`,
       options: [{ production: true }],
-      before: makeSetup({ dependencies: { eslint: "" } }),
+      before: makeBefore({ dependencies: { eslint: "" } }),
     },
     {
       name: "regular import of required peer dependency",
       code: `import {} from "eslint"`,
-      before: makeSetup({ peerDependencies: { eslint: "" } }),
+      before: makeBefore({ peerDependencies: { eslint: "" } }),
     },
     {
       name: "type import of optional peer dependency",
       code: `import type {} from "eslint"`,
-      before: makeSetup({
+      before: makeBefore({
         peerDependencies: { eslint: "" },
         peerDependenciesMeta: { eslint: { optional: true } },
       }),
@@ -43,58 +43,58 @@ tester.run("dependencies", rule, {
       name: "type import of unlisted type only dependency",
       code: `import type {} from "eslint"`,
       options: [{ typeOnly: ["eslint"] }],
-      before: makeSetup({ dependencies: {} }),
+      before: makeBefore({ dependencies: {} }),
     },
     {
       name: "type import of prod dependency",
       code: `import type {} from "eslint"`,
-      before: makeSetup({ dependencies: { eslint: "" } }),
+      before: makeBefore({ dependencies: { eslint: "" } }),
     },
     {
       name: "import a path of prod dependency",
       code: `import {} from "eslint/something/useful.js"`,
-      before: makeSetup({ dependencies: { eslint: "" } }),
+      before: makeBefore({ dependencies: { eslint: "" } }),
     },
     {
       name: "import of scoped required peer dependency",
       code: `import {} from "@eslint/js/something"`,
-      before: makeSetup({ peerDependencies: { "@eslint/js": "" } }),
+      before: makeBefore({ peerDependencies: { "@eslint/js": "" } }),
     },
     {
       name: "import of local path",
       code: `import {} from "./some/path.js"`,
-      before: makeSetup({}),
+      before: makeBefore({}),
     },
     {
       name: "import of built-in module",
       code: `import { readFileSync } from "node:fs"`,
-      before: makeSetup({}),
+      before: makeBefore({}),
     },
     {
       name: "import of something explicitly ignored by regex",
       code: `import {} from "fancy-module"`,
       options: [{ ignore: ["^fancy-\\w+$"] }],
-      before: makeSetup({}),
+      before: makeBefore({}),
     },
   ],
   invalid: [
     {
       name: "regular import of unlisted dependency",
       code: `import {} from "eslint"`,
-      before: makeSetup({ dependencies: {} }),
+      before: makeBefore({ dependencies: {} }),
       errors: [{ messageId: "prohibited" }],
     },
     {
       name: "regular import of explicitly disabled prod dependency",
       code: `import {} from "eslint"`,
       options: [{ production: false }],
-      before: makeSetup({ dependencies: { eslint: "" } }),
+      before: makeBefore({ dependencies: { eslint: "" } }),
       errors: [{ messageId: "prohibited" }],
     },
     {
       name: "regular import of optional peer dependency",
       code: `import {} from "eslint"`,
-      before: makeSetup({
+      before: makeBefore({
         peerDependencies: { eslint: "" },
         peerDependenciesMeta: { eslint: { optional: true } },
       }),
@@ -104,20 +104,20 @@ tester.run("dependencies", rule, {
       name: "regular import of unlisted type only dependency",
       code: `import {} from "eslint"`,
       options: [{ typeOnly: ["eslint"] }],
-      before: makeSetup({}),
+      before: makeBefore({}),
       errors: [{ messageId: "typeOnly" }],
     },
     {
       name: "import of built-in module when missing in ignore list",
       code: `import {} from "node:fs"`,
       options: [{ ignore: ["^fancy-\\w+$"] }],
-      before: makeSetup({}),
+      before: makeBefore({}),
       errors: [{ messageId: "prohibited" }],
     },
     {
       name: "demo",
       code: `import {factory} from "typescript"; import {format} from "prettier";`,
-      before: makeSetup({
+      before: makeBefore({
         devDependencies: { typescript: "^5" },
         peerDependencies: { prettier: "^3" },
         peerDependenciesMeta: { prettier: { optional: true } },
