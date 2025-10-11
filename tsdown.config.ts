06da54d0abe645ae3a25fc4bdec24e33266aedec
Anna Bocharova <anna.bocharova@grover.com>
2025-10-11 10:37:35 +0200

    tsdown config: rm defaults.

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  format: "esm",
   entry: "./src/index.ts",
-  sourcemap: false,
-  clean: true,
-  dts: true,
   minify: true,
   skipNodeModulesBundle: true,
   attw: {
