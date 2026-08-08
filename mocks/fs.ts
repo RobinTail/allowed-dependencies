20a10a0250a4395a92bfde8986818cde24c35ea3
Anna Bocharova <robin_tail@me.com>
2026-08-08 18:06:31 +0200

    Using TypeScript 7. (#132)
    * Using TypeScript 7.
    
    * type only import in mock.

--- mocks/fs.ts
+++ mocks/fs.ts
@@ -1,6 +1,6 @@
-import { vi } from "vitest";
+import { vi, type Mock } from "vitest";
 
-export const readerMock = vi.fn();
+export const readerMock: Mock = vi.fn();
 
 vi.mock("node:fs", async () => {
   const actual = await vi.importActual<typeof import("node:fs")>("node:fs");
